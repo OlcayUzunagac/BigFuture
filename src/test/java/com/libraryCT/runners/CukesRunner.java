c117fe45a17c326b0b3468d2b74e00c8bb06b3a0
OlcayUzunagac <olcayuzunagac@gmail.com>
2020-10-27 16:07:28 +0100

    Project started, directories and files added

---
+++ src/test/java/com/libraryCT/runners/CukesRunner.java
@@ -0,0 +1,16 @@
+package com.libraryCT.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json", "html:target/default-html-reports"},
+        features = "src/test/resources/features",
+        glue = "com/libraryCT/step_definitions",
+        dryRun =false,
+        tags = ""
+)
+public class CukesRunner {
+}
