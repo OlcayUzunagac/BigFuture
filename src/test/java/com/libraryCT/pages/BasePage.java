65198492112440072df67edf6387fcc6eb46d7e3
username <username@email.com>
2020-11-02 10:31:54 +0100

    BasaPage created

---
+++ src/test/java/com/libraryCT/pages/BasePage.java
@@ -0,0 +1,51 @@
+package com.libraryCT.pages;
+
+import com.libraryCT.utilities.BrowserUtils;
+import com.libraryCT.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class BasePage {
+
+    public BasePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(css = "li.nav-item")
+    public List<WebElement> menuOptions;
+
+    @FindBy(css = "span.title")
+    public WebElement pageTitle;
+
+    @FindBy(linkText = "Log Out")
+    public WebElement logOutLink;
+
+    @FindBy(css = "#navbarDropdown > span")
+    public WebElement userName;
+
+
+
+    public String getPageTitle() {
+        BrowserUtils.waitForStaleElement(pageTitle);
+        return pageTitle.getText();
+    }
+
+    public String getUserName(){
+        BrowserUtils.waitForVisibility(userName, 5);
+        return userName.getText();
+    }
+
+    public void logOut(){
+        //BrowserUtils.waitFor(2);
+        BrowserUtils.clickWithJS(userName);
+        BrowserUtils.clickWithJS(logOutLink);
+    }
+
+
+}
